0b4af3e865f4e0e7e4b27df01259577f675e5c6e
Ariel999 <ariel_aaronph@hotmail.com>
2020-05-15 17:50:21 -0500

    Carrito

--- masterbeer/src/Components/Carrito/Carrito.js
+++ masterbeer/src/Components/Carrito/Carrito.js
@@ -1,21 +1,71 @@
 import React,{useEffect,useReducer,useState} from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { Table } from "reactstrap";
+import { loadBotellas } from "../../Redux/Actions/BotellaActions";
+import useModal from "../Common/ModalMB/useModalObject";
+
+
+
+function CarritoArray({ CarritoArray ,toggle,changeObject}){
+    const dispatch = useDispatch();
+    return CarritoArray.map((elements) => {
+      return (
+          
+        <tr key={elements._id}>
+          <td>{elements.Nombre}</td>
+          <td>{elements.Precio}</td>
+          <td>Cantidad</td>
+          <td>
+            <button>ELIMINAR</button>
+          </td>
+          <td>
+  
+          </td>
+        </tr>
+  
+      );
+    });
+  };
 
 function Carrito(){
-return( 
-    <div className="Carrito">
-        <div className="row">
-            <div className="col-8 carritoContent justify-content-center">
-                holakks
-            </div>
-            <div className="col-4 carritoCuenta">
-                <h5>Subtotal</h5>
-                <hr></hr>
-                <h4>Total</h4>
-                <button>Proceder al pago</button>
+    const Botellas = useSelector((state) => state.Botellas);
+    const { isShowing, toggle ,Obj,changeObject} = useModal();
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        if(Botellas.length===0) dispatch(loadBotellas());
+    });
+    
+    return( 
+        <div className="Carrito" id="Carrito">
+            <div className="row">
+                <div className="col-8 carritoContent justify-content-center">
+                    
+                    <Table dark className="col">
+                        <thead>
+                            <tr>
+                            <th>Descripción</th>
+                            <th>Precio</th>
+                            <th>Cantidad</th>
+                            
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <CarritoArray CarritoArray={Botellas}  toggle={toggle} changeObject={changeObject} dispatch={dispatch}/>
+                            </tbody>
+                    </Table>
+                </div>
+                <div className="col-4 carritoCuenta">
+                    <h5>Subtotal</h5>
+                    
+                    <hr></hr>
+                    
+                    <h4>Total MXN</h4>
+                    <button>Proceder al pago</button>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default Carrito;
